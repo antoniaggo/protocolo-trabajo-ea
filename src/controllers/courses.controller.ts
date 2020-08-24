3733ce281a3f9d6f42debffccc41d45065b784ba
Antonia Gallardo <antonia.gallardo.gomez@gmail.com>
2020-08-24 11:49:12 +0200

    bug en controllador status 200

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -4,6 +4,6 @@ import Course from "../models/course";
 export const courses = async (req: Request, res: Response) => {
 
     const results = await Course.find({});
-    return res.status(400).json(results);
+    return res.status(200).json(results);
 
 }
